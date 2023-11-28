1f8d96cb2258175da6a5fc992f7f3df402151aca
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-11-28 15:25:42 +0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from flask import render_template
 from app import app
 def index():
+    user = {"nickname": "Pavel"}
+    posts = [
+        {
+            "author": {"nickname": "John"},
+            "body": 'Beatiful day in Russia'
+        },
+        {
+            "author": {"nickname": "Riddik"},
+            "body": 'The Spiderman movie was is cool'
+        }
+    ]
     return render_template("index.html",
                           title = "Home",
-                          user = user)
+                          user = user,
+                          posts = posts)
