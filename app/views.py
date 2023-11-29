9eb674d2828294fb669b25689eb094a5a6ccb0c3
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-11-29 08:30:27 +0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -77,6 +77,21 @@ def logout():
 def edit():
     form = EditForm(g.user.nickname)
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        { 'author': user, 'body': 'Test post #1' },
+        { 'author': user, 'body': 'Test post #2' }
+    ]
+    return render_template('user.html',
+        user = user,
+        posts = posts)
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
