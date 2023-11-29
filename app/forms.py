2f6adac3c18ab1030e5198c4f1c653e5987755c5
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-11-29 10:07:24 +0400

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import Required, Length
 from app.models import User
 
 class LoginForm(Form):
