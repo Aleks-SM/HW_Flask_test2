cb7cfe6a9561c94622c5c81ba2b7520b4d696689
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-11-29 10:18:09 +0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm
+from forms import LoginForm, EditForms
 from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
@@ -80,6 +80,18 @@ def logout():
 @login_required
 def edit():
     form = EditForm(g.user.nickname)
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html',
+        form = form)
 
 @app.route('/user/<nickname>')
 @login_required
