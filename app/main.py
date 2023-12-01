43aa5b875f501512ddd75db7f916f4ce2497a70e
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-12-01 12:45:49 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from flask import flask
+from database import Database
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    bd_test = Database()
+    engine = sq.create_engine(bd_test.create_conect())
+    bd_test.create_tables(engine)
