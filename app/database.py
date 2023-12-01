d2f4bb2ee8f8ae1fc04b348255bdb15ae28b1539
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-12-01 12:42:35 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,14 +4,14 @@ from app.config import POSTGRES_DB, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_P
 class Database:
     def __init__(self):
         self.bd = postgresql
-        self.host = POSTGRES_HOST
+        self.bd_host = POSTGRES_HOST
         self.bd_port = POSTGRES_PORT
         self.bd_name = POSTGRES_DB
         self.bd_username = POSTGRES_USER
         self.bd_pass = POSTGRES_PASSWORD
 
     def create_conect(self):
-        dsn = "{}://{}:{}@{}:{}/{}".format(self.bd, self.bd_username, self.bd_pass, self.host, self.bd_port, self.bd_name)
+        dsn = "{}://{}:{}@{}:{}/{}".format(self.bd, self.bd_username, self.bd_pass, self.bd_host, self.bd_port, self.bd_name)
         return dsn
 
     def create_tables(self, engine):
