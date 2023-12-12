06166f2b5450b093b2cf1d78ac30efe53ad9c5a3
AleksSM <aleks-m83@mail.ru>
2023-12-12 18:13:52 +0400

    fix bugs

--- app/models.py
+++ app/models.py
@@ -1,22 +1,14 @@
-import os
 import datetime
 from sqlalchemy import String, DateTime, func, ForeignKey
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Mapped, mapped_column
+from config import POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST
 
 
-
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "app")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "app")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5431")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
 
 PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
 engine = create_engine(PG_DSN)
 Session = sessionmaker(bind=engine)
 
@@ -28,9 +20,9 @@ class User(Base):
     __tablename__ = "user"
 
     user_id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
     password: Mapped[str] = mapped_column(String(100), nullable=False)
-    email: Mapped[str] = mapped_column(String(50), unique=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
     registration_time: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
 
     @property
@@ -47,9 +39,9 @@ class Advertisement(Base):
 
     adv_id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(20), nullable=False)
-    description: Mapped[str] = mapped_column(String(240), nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
     date_post: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
-    owner_id: Mapped[int] = mapped_column(ForeignKey("user.user_id", ondelete='CASCADE'), nullable=False )
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.user_id", ondelete='CASCADE'), nullable=False)
 
     user = relationship("User", backref="user")
 
