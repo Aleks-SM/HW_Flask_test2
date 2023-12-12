0088f0b70797c7099458bacb119625a1fefd5697
AleksSM <aleks-m83@mail.ru>
2023-12-12 18:35:43 +0400

    add scrypt

--- app/tools.py
+++ app/tools.py
@@ -1,11 +1,21 @@
 from pydantic import ValidationError
 from shema import SHEMA_CLASS
 from error import HttpError
+from flask_scrypt import generate_password_hash, generate_random_salt, check_password_hash
 
-def validate(schema_cls: SHEMA_CLASS, json_data: dict | list):
+def validate(schema_cls, json_data: dict | list):
     try:
         return schema_cls(**json_data).dict(exclude_unset=True)
     except ValidationError as er:
         error = er.errors()[0]
         error.pop("ctx", None)
+        error.pop("url", None)
         raise HttpError(400, error)
+
+def hash_password(user_password):
+    salt = generate_random_salt().decode()
+    password = generate_password_hash(user_password, salt).decode()
+    return {'salt': salt, 'password': password}
+
+def check_password(password, hash, salt):
+    return check_password_hash(password.encode(), hash.encode(), salt.encode())
