80d46dd1df508ad2c8cd46b4decd04077b06ea10
AleksSM <aleks-m83@mail.ru>
2023-12-13 19:26:36 +0400

    fix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import flask
+from flask import request, jsonify
 
-from models import Session, User
+from models import Session
 from error import HttpError
+from views import UserView, LoginView
 
-from tools import validate
 
-
-app = flask.Flask("app")
+app = flask.Flask(__name__)
 
 
 @app.before_request
@@ -32,7 +32,7 @@ user_view = UserView.as_view("user_view")
 
 app.add_url_rule("/users/<int:user_id>", view_func=user_view, methods=["GET", "PATCH", "DELETE"])
 app.add_url_rule("/users", view_func=user_view, methods=["POST"])
-app.add_url_rule("/login", view_func=login_view, methods=["POST"])
+app.add_url_rule("/login", view_func=LoginView.as_view('login_view'), methods=["POST"])
 
 if __name__ == "__main__":
     app.run()
